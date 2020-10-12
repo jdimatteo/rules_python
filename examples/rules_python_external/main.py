b37173ac5bcedc202953705e1afce891c6947f63
John DiMatteo <jdimatteo@verily.com>
2020-10-12 15:11:21 -0600

    confirmed correct version of six is being used

--- examples/rules_python_external/main.py
+++ examples/rules_python_external/main.py
@@ -1,7 +1,14 @@
+import six
 import boto3
 
 def the_dir():
     return dir(boto3)
+
+def boto3_version():
+    return boto3.__version__
+
+def six_version():
+    return six.__version__
 
 if __name__ == "__main__":
     print(the_dir())
--- examples/rules_python_external/test.py
+++ examples/rules_python_external/test.py
@@ -5,5 +5,11 @@ class ExampleTest(unittest.TestCase):
     def test_main(self):
         self.assertIn("set_stream_logger", main.the_dir())
 
+    def test_boto3_version(self):
+        self.assertEqual("1.14.51", main.boto3_version())
+
+    def test_six_version(self):
+        self.assertEqual("1.15.0", main.six_version())
+
 if __name__ == '__main__':
   unittest.main()
