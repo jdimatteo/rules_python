770c4a2c8f73e519699b405df6ef829e6c62806b
John DiMatteo <jdimatteo@verily.com>
2020-10-13 14:57:06 -0600

    Simplify error example
    jdimatteo@hcgr:~/dev/external/rules_python/examples/rules_python_external$ bazel build main
    ERROR: /usr/local/google/home/jdimatteo/.cache/bazel/_bazel_jdimatteo/ceed6f91eeddb0cbb65f651772fe0c51/external/com_google_protobuf/BUILD:860:17: no such target '//external:six': target 'six' not declared in package 'external' defined by /usr/local/google/home/jdimatteo/dev/external/rules_python/examples/rules_python_external/WORKSPACE and referenced by '@com_google_protobuf//:protobuf_python'
    ERROR: Analysis of target '//:main' failed; build aborted: Analysis failed
    INFO: Elapsed time: 0.090s
    INFO: 0 processes.
    FAILED: Build did NOT complete successfully (1 packages loaded, 1 target configured)
    jdimatteo@hcgr:~/dev/external/rules_python/examples/rules_python_external$

--- examples/rules_python_external/main.py
+++ examples/rules_python_external/main.py
@@ -1,14 +1,1 @@
-import six
-import boto3
-
-def the_dir():
-    return dir(boto3)
-
-def boto3_version():
-    return boto3.__version__
-
-def six_version():
-    return six.__version__
-
-if __name__ == "__main__":
-    print(the_dir())
+print("Hello World")
